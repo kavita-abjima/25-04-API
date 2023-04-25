7d0455555da29b124f5e4c88803e5bcce9eed1fc
Kavita <kavita.jangir@abjima.com>
2023-04-25 18:57:45 +0530

    API

---
+++ CityInfo3.API/CitiesDataStore.cs
@@ -0,0 +1,40 @@
+﻿using CityInfo3.API.Models;
+
+namespace CityInfo3.API
+{
+        public class CitiesDataStore
+        {
+
+            public List<CityDto> cities { get; set; }
+            public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+            public CitiesDataStore()
+            {
+                cities = new List<CityDto>();
+                {
+                    new CityDto()
+                    {
+                        Id = 1,
+                        Name = "New York",
+                        Discription = "The one with the big park",
+
+                    };
+                    new CityDto()
+                    {
+                        Id = 2,
+                        Name = "AntWrep",
+                        Discription = "The one with the big Tower"
+
+                    };
+                    new CityDto()
+                    {
+                        Id = 3,
+                        Name = "Paris",
+                        Discription = "The one with the big Theater"
+
+                    };
+                }
+            }
+            
+        }
+}
